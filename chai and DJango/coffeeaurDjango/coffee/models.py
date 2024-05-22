ba10ed5812d10215b1e83e2274d47f34138e752f
Karush2807 <arushkarnatak1881@gmail.com>
2024-05-22 23:32:41 +0530

    added modelsgit add .

--- chai and DJango/coffeeaurDjango/coffee/models.py
+++ chai and DJango/coffeeaurDjango/coffee/models.py
@@ -1,3 +1,10 @@
+#production level pr bhi jo hm models bnayenge vo, app pr hi bnegi!!
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class coffee_varieties(models.Model):
+    name=models.CharField(max_length=100)#this sets max length to 100
+    image=models.ImageField(upload_to='coffees/') #ese hi images upload hoti hai
+    date=models.models.DateTimeField(default=timezone.now)
+    

d4642a9802eed984fc66ca51b2f546e543f449d1
Karush2807 <arushkarnatak1881@gmail.com>
2024-06-02 22:48:06 +0530

    created urls and vie for Django forms

--- chai and DJango/coffeeaurDjango/coffee/urls.py
+++ chai and DJango/coffeeaurDjango/coffee/urls.py
@@ -5,9 +5,8 @@ from . import views
 # localhost:8000/coffee/
 # localhost:8000/coffee/order
 urlpatterns = [
-    
     path('', views.all_coffee, name='all_coffee'),
     path('<int:coffee_id>/', views.coffee_detail, name='coffee_detail'),
-
+    path('coffeeStores/', views.coffee_Store_view, name='coffee_Store'),
 
 ]
--- chai and DJango/coffeeaurDjango/coffee/views.py
+++ chai and DJango/coffeeaurDjango/coffee/views.py
@@ -12,3 +12,5 @@ def coffee_detail(request, coffee_id):
     coffee=get_object_or_404(coffee_varieties, pk=coffee_id) #ye db pr query krega, arryas aayenge
     return render(request, 'coffee/coffee_details.html', {'coffee':coffee})
 
+def coffee_Store_view(request):
+    return render(request, 'coffee/coffee_stores.html')
